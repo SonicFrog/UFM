b72dc05eaa7de0fe4dfea75c559082c4d2a8280b
Ogier Bouvier <ogier.bouvier@epfl.ch>
2013-01-10 22:48:18 +0100

    Correction d'un bug dans PropertyReader. Le contenu du fichier properties était écrite plusieurs dans le fichier

--- src/com/unilabs/io/PropertyReader.java
+++ src/com/unilabs/io/PropertyReader.java
@@ -10,41 +10,46 @@ import java.util.Properties;
 
 public class PropertyReader {
 
-	public static String COMMENT = "N'";
+	public static String COMMENT = "Fichier de configuration pour UFM\nN'éditez de fichier que si vous savez ce que vous faites, " +
+			"vous risquez de corrompre votre installation!";
 	private File propertyFile;
 	private Properties prop;
 	private OutputStream out;
-	
+
 	public PropertyReader(String path) throws IOException {
 		propertyFile = new File(path);
 		out = new BufferedOutputStream(new FileOutputStream(propertyFile));
 		init();
 	}
-	
+
 	public PropertyReader(File file) throws IOException {
 		propertyFile = file;
 		init();
 	}
-	
+
 	public PropertyReader(Properties p) {
 		prop = p;
 	}
-	
+
 	private void init() throws IOException {
 		prop = new Properties();
 		prop.load(new FileInputStream(propertyFile));
 	}
-	
+
 	public String getProperty(String key) {
 		return prop.getProperty(key);
 	}
-	
+
 	public String getProperty(String key, String defaultValue) {
 		return prop.getProperty(key, defaultValue);
 	}
-	
-	public void writeProperty(String key, String value) throws IOException {
+
+	public void writeProperty(String key, String value) {
+		System.out.println("Setting key " + key + " to " + value);
 		prop.setProperty(key, value);
+	}
+	
+	public void flush() throws IOException {
 		prop.store(out, COMMENT);
 	}
 }
